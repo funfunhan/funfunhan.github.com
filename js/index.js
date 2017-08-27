006052036d4ae43aa34ce033ee067fda169d9458
funfunhan <woohwan5810@naver.com>
2017-08-27 16:53:48 +0900

    renewal portfolio
    새로 작업

--- js/index.js
+++ js/index.js
@@ -1,221 +1,176 @@
-(function() {
+$(window).load( function() {
+		$('#portfolio').BlocksIt({
+			numOfCol: 4,
+			offsetX: 0,
+			offsetY: 5,
+      blockElement:".grid"
+		});
 
-    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
-
-    // Main
-    initHeader();
-    initAnimation();
-    addListeners();
-
-    function initHeader() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        target = {x: width/2, y: height/5};
-
-        largeHeader = document.getElementById('large-header');
-        largeHeader.style.height = 300+'px';
-
-        canvas = document.getElementById('demo-canvas');
-        canvas.width = width;
-        canvas.height = 300;
-        ctx = canvas.getContext('2d');
-
-        // create points
-        points = [];
-        for(var x = 0; x < width; x = x + width/40) {
-            for(var y = 0; y < height; y = y + height/60) {
-                var px = x + Math.random()*width/40;
-                var py = y + Math.random()*height/60;
-                var p = {x: px, originX: px, y: py, originY: py };
-                points.push(p);
-            }
-        }
-
-        // for each point find the 5 closest points
-        for(var i = 0; i < points.length; i++) {
-            var closest = [];
-            var p1 = points[i];
-            for(var j = 0; j < points.length; j++) {
-                var p2 = points[j];
-                if(!(p1 == p2)) {
-                    var placed = false;
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(closest[k] == undefined) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-
-                    for(var k = 0; k < 5; k++) {
-                        if(!placed) {
-                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
-                                closest[k] = p2;
-                                placed = true;
-                            }
-                        }
-                    }
-                }
-            }
-            p1.closest = closest;
-        }
-
-        // assign a circle to each point
-        for(var i in points) {
-            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
-            points[i].circle = c;
-        }
+    var header_img = document.getElementsByClassName("headerimgholder");
+    var width = $(window).width() -55;
+    var howmany = (width/150 -1)*2;
+    for(var i=0; i<howmany; i++){
+      header_img[i].style.display = "inline-block";
     }
+	});
 
-    // Event handling
-    function addListeners() {
-        if(!('ontouchstart' in window)) {
-            window.addEventListener('mousemove', mouseMove);
-        }
-        window.addEventListener('scroll', scrollCheck);
-        window.addEventListener('resize', resize);
-    }
-
-    function mouseMove(e) {
-        var posx = posy = 0;
-        if (e.pageX || e.pageY) {
-            posx = e.pageX;
-            posy = e.pageY;
-        }
-        else if (e.clientX || e.clientY)    {
-            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
-            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
-        }
-        target.x = posx;
-        target.y = posy;
-    }
-
-    function scrollCheck() {
-        if(document.body.scrollTop > height) animateHeader = false;
-        else animateHeader = true;
-    }
-
-    function resize() {
-        width = window.innerWidth;
-        height = window.innerHeight;
-        largeHeader.style.height = 300+'px';
-        canvas.width = width;
-        canvas.height = 300+'px';
-    }
-
-    // animation
-    function initAnimation() {
-        animate();
-        for(var i in points) {
-            shiftPoint(points[i]);
-        }
-    }
-
-    function animate() {
-        if(animateHeader) {
-            ctx.clearRect(0,0,width,height);
-            for(var i in points) {
-                // detect points in range
-                if(Math.abs(getDistance(target, points[i])) < 4000) {
-                    points[i].active = 0.3;
-                    points[i].circle.active = 0.6;
-                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
-                    points[i].active = 0.1;
-                    points[i].circle.active = 0.3;
-                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
-                    points[i].active = 0.02;
-                    points[i].circle.active = 0.1;
-                } else {
-                    points[i].active = 0;
-                    points[i].circle.active = 0;
-                }
-
-                drawLines(points[i]);
-                points[i].circle.draw();
-            }
-        }
-        requestAnimationFrame(animate);
-    }
-
-    function shiftPoint(p) {
-        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
-            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
-            onComplete: function() {
-                shiftPoint(p);
-            }});
-    }
-
-    // Canvas manipulation
-    function drawLines(p) {
-        if(!p.active) return;
-        for(var i in p.closest) {
-            ctx.beginPath();
-            ctx.moveTo(p.x, p.y);
-            ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(220,220,220,'+ p.active+')';
-            ctx.stroke();
-        }
-    }
-
-    function Circle(pos,rad,color) {
-        var _this = this;
-
-        // constructor
-        (function() {
-            _this.pos = pos || null;
-            _this.radius = rad || null;
-            _this.color = color || null;
-        })();
-
-        this.draw = function() {
-            if(!_this.active) return;
-            ctx.beginPath();
-            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(220,220,220,'+ _this.active+')';
-            ctx.fill();
-        };
-    }
-
-    // Util
-    function getDistance(p1, p2) {
-        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
-    }
-
-})();
-
-
-
-
-$(document).ready(function() {
-			var $gallery = $('#gallery');
-			var $boxes = $('.revGallery-anchor');
-			$boxes.hide();
-
-			$gallery.imagesLoaded( {background: true}, function() {
-    		$boxes.fadeIn();
-
-				$gallery.isotope({
-					// options
-					sortBy : 'original-order',
-					layoutMode: 'fitRows',
-					itemSelector: '.revGallery-anchor',
-					stagger: 30,
-				});
-			});
-
-		 	$('button').on( 'click', function() {
-		 		 var filterValue = $(this).attr('data-filter');
-			  	$('#gallery').isotope({ filter: filterValue });
-			  	$gallery.data('lightGallery').destroy(true);
-			    $gallery.lightGallery({
-			      	selector: filterValue.replace('*','')
-			    });
-			});
+//window resize
+$(window).resize(function() {
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+  var header_img = document.getElementsByClassName("headerimgholder");
+  var width = $(window).width() -55;
+  var howmany = (width/150 -1)*2;
+  for(var j=0;j<header_img.length;j++){
+    header_img[j].style.display = "none";
+  }
+  for(var i=0; i<howmany; i++){
+    header_img[i].style.display = "inline-block";
+  }
 });
 
 //button active mode
 $('.button').click(function(){
-    $('.button').removeClass('is-checked');
-    $(this).addClass('is-checked');
+    $('.button').removeClass('btn_active');
+    $(this).addClass('btn_active');
 });
+function select_ALL(){
+  var all = document.getElementsByClassName("ALL");
+  for(var a=0; a<all.length; a++){
+    all[a].classList.add("grid");
+    all[a].style.display = "block";
+  }
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+}
+
+function select_University(){
+  var all = document.getElementsByClassName("ALL");
+  var university = document.getElementsByClassName("university");
+
+  for(var a=0; a<all.length; a++){
+    all[a].classList.remove("grid");
+    all[a].style.display = "none";
+  }
+
+  for(var b=0; b<university.length; b++){
+    university[b].classList.add("grid");
+    university[b].style.display = "block";
+  }
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+}
+function select_Activity(){
+  var all = document.getElementsByClassName("ALL");
+  var activity = document.getElementsByClassName("activity");
+
+  for(var a=0; a<all.length; a++){
+    all[a].classList.remove("grid");
+    all[a].style.display = "none";
+  }
+
+  for(var b=0; b<activity.length; b++){
+    activity[b].classList.add("grid");
+    activity[b].style.display = "block";
+  }
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+}
+function select_Devlop(){
+  var all = document.getElementsByClassName("ALL");
+  var develop = document.getElementsByClassName("develop");
+
+  for(var a=0; a<all.length; a++){
+    all[a].classList.remove("grid");
+    all[a].style.display = "none";
+  }
+
+  for(var b=0; b<develop.length; b++){
+    develop[b].classList.add("grid");
+    develop[b].style.display = "block";
+  }
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+}
+function select_LikeLion(){
+  var all = document.getElementsByClassName("ALL");
+  var LikeLion_active = document.getElementsByClassName("LikeLion_active");
+
+  for(var a=0; a<all.length; a++){
+    all[a].classList.remove("grid");
+    all[a].style.display = "none";
+  }
+
+  for(var b=0; b<LikeLion_active.length; b++){
+    LikeLion_active[b].classList.add("grid");
+    LikeLion_active[b].style.display = "block";
+  }
+  $('#portfolio').BlocksIt({
+    numOfCol: 4,
+    offsetX: 0,
+    offsetY: 5,
+    blockElement:".grid"
+  });
+}
+
+
+// modal
+function display_modal(i){
+  var modal = document.getElementsByClassName('modal');
+  var body = document.body;
+  modal[i].style.display = "block";
+  body.style.overflow = "hidden";
+
+}
+function close_modal(j){
+  var modal = document.getElementsByClassName('modal');
+  var body = document.body;
+  modal[j].style.display = "none";
+  body.style.overflow = "auto";
+}
+
+
+//header
+setimer();
+function setimer(){
+  var hover_infinite = setTimeout(
+    function(){ hoverfunc();
+      console.log();
+   }, 3000);
+ }
+
+function hoverfunc(){
+  var header_img = document.getElementsByClassName("headerimgholder");
+  var width = $(window).width() -55;
+  var howmany = (width/150 -1)*2;
+  for(var j=0;j<header_img.length;j++){
+    header_img[j].classList.remove("header_active");
+  }
+  var index = Math.floor((Math.random() * howmany));
+  header_img[index].classList.add("header_active");
+  setimer();
+}
+
+window.onhashchange = function() {
+  window.history.back();
+};
