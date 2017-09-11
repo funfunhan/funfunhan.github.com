e73deefbc5ecb051b423ce766c94da36dff547f5
funfunhan <woohwan5810@naver.com>
2017-09-11 13:21:59 +0900

    fix js

--- js/index.js
+++ js/index.js
@@ -16,6 +16,9 @@ $(window).resize(function() {
   });
   var header_img = document.getElementsByClassName("headerimgholder");
   var width = $(window).width() -55;
+  if (width > 1550){
+    width = 1500;
+  }
   var howmany = (width/150 -1)*2;
   for(var j=0;j<header_img.length;j++){
     header_img[j].style.display = "none";
